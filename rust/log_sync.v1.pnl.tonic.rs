5f37d7bb51920395d0cebbd9441cea5e4d19a126
MoMentum99 <kmc2048@gmail.com>
2025-07-11 12:27:18 +0900

    ci: add cargo.toml

--- rust/log_sync.v1.pnl.tonic.rs
+++ rust/log_sync.v1.pnl.tonic.rs
@@ -130,19 +130,17 @@ pub mod pnl_service_server {
     }
     #[derive(Debug)]
     pub struct PnlServiceServer<T: PnlService> {
-        inner: _Inner<T>,
+        inner: Arc<T>,
         accept_compression_encodings: EnabledCompressionEncodings,
         send_compression_encodings: EnabledCompressionEncodings,
         max_decoding_message_size: Option<usize>,
         max_encoding_message_size: Option<usize>,
     }
-    struct _Inner<T>(Arc<T>);
     impl<T: PnlService> PnlServiceServer<T> {
         pub fn new(inner: T) -> Self {
             Self::from_arc(Arc::new(inner))
         }
         pub fn from_arc(inner: Arc<T>) -> Self {
-            let inner = _Inner(inner);
             Self {
                 inner,
                 accept_compression_encodings: Default::default(),
@@ -205,7 +203,6 @@ pub mod pnl_service_server {
             Poll::Ready(Ok(()))
         }
         fn call(&mut self, req: http::Request<B>) -> Self::Future {
-            let inner = self.inner.clone();
             match req.uri().path() {
                 "/log_sync.v1.pnl.PnlService/ListPnlSummaries" => {
                     #[allow(non_camel_case_types)]
@@ -225,7 +222,7 @@ pub mod pnl_service_server {
                         ) -> Self::Future {
                             let inner = Arc::clone(&self.0);
                             let fut = async move {
-                                (*inner).list_pnl_summaries(request).await
+                                <T as PnlService>::list_pnl_summaries(&inner, request).await
                             };
                             Box::pin(fut)
                         }
@@ -236,7 +233,6 @@ pub mod pnl_service_server {
                     let max_encoding_message_size = self.max_encoding_message_size;
                     let inner = self.inner.clone();
                     let fut = async move {
-                        let inner = inner.0;
                         let method = ListPnlSummariesSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
@@ -258,8 +254,11 @@ pub mod pnl_service_server {
                         Ok(
                             http::Response::builder()
                                 .status(200)
-                                .header("grpc-status", "12")
-                                .header("content-type", "application/grpc")
+                                .header("grpc-status", tonic::Code::Unimplemented as i32)
+                                .header(
+                                    http::header::CONTENT_TYPE,
+                                    tonic::metadata::GRPC_CONTENT_TYPE,
+                                )
                                 .body(empty_body())
                                 .unwrap(),
                         )
@@ -278,16 +277,6 @@ pub mod pnl_service_server {
                 max_decoding_message_size: self.max_decoding_message_size,
                 max_encoding_message_size: self.max_encoding_message_size,
             }
-        }
-    }
-    impl<T: PnlService> Clone for _Inner<T> {
-        fn clone(&self) -> Self {
-            Self(Arc::clone(&self.0))
-        }
-    }
-    impl<T: std::fmt::Debug> std::fmt::Debug for _Inner<T> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{:?}", self.0)
         }
     }
     impl<T: PnlService> tonic::server::NamedService for PnlServiceServer<T> {
