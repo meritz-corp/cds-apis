e50b497fd473c0f30649299934d56809fb9c0c8b
Covy <47184034+MoMentum99@users.noreply.github.com>
2025-09-07 11:55:09 +0900

    feat: add kdo module and include market definitions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,5 +3,14 @@ pub mod log_sync {
         pub mod pnl {
             include!("log_sync.v1.pnl.rs");
         }
+        // pub mod order_log {
+        //     include!("log_sync.v1.order_log.rs");
+        // }
+    }
+}
+
+pub mod kdo {
+    pub mod v1 {
+        include!("kdo.v1.market.rs");
     }
 }
